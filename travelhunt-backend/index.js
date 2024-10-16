813171e368972b366ef12c9942d5510f1ae9bf6c
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-10-16 11:39:23 +0530

    signup/login added

--- travelhunt-backend/index.js
+++ travelhunt-backend/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const authRoutes = require('./routes/auth');
 dotenv.config();
 
 const app = express();
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //routes
-// app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
